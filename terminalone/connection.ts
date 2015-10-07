fe04ae2d210e369bfc71e2e800e1eea18d709593
Fotios Tzellos <ftzellos@mediamath.com>
2015-10-07 11:54:56 +0100

    entity saving

--- terminalone/connection.ts
+++ terminalone/connection.ts
@@ -1,17 +1,20 @@
 /// <reference path="../typings/node/node.d.ts"/>
 
+var request = require('request');
+var querystring = require('querystring');
+var api_url = "https://api.mediamath.com/api/v2.0/";
+
 class T1Connection {
     private config;
-    private request = require('request');
-    private headers = {'accept':'application/vnd.mediamath.v1+json'};
+    private accept_headers = {'accept': 'application/vnd.mediamath.v1+json'};
 
     constructor(config, cb) {
         this.config = config;
-        var login = this.request.post(
+        var login = request.post(
             {
                 jar: true,
-                url: "https://api.mediamath.com/api/v2.0/login",
-                headers: this.headers,
+                url: api_url + "login",
+                headers: this.accept_headers,
                 form: {
                     user: config.user,
                     password: config.password,
@@ -30,11 +33,17 @@ class T1Connection {
             });
     }
 
-    public get(endpoint: string, cb) {
-        this.request.get( {
+    private copyHeaders = function (source, sink) {
+        Object.keys(source).forEach(function (key) {
+            sink[key] = source[key];
+        });
+    };
+
+    public get(endpoint:string, cb) {
+        request.get({
                 jar: true,
-                headers: this.headers,
-                url: "https://api.mediamath.com/api/v2.0/" + endpoint,
+                headers: this.accept_headers,
+                url: api_url + endpoint,
             },
             function (error, response, body) {
                 //Check for error
@@ -48,8 +57,38 @@ class T1Connection {
             });
     }
 
-    public getSession(cb): void {
+    public getSession(cb):void {
         return this.get('session', cb);
+    }
+
+    public post(endpoint:string, form:Object, cb) {
+
+        var formData = querystring.stringify(form);
+        var contentLength = formData.length;
+
+        var post_headers = {
+            'Content-Length': contentLength,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+
+        this.copyHeaders(this.accept_headers, post_headers);
+
+        request.post({
+                jar: true,
+                headers: post_headers,
+                url: api_url + endpoint,
+                body: formData
+            },
+            function (error, response, body) {
+                //Check for error
+                if (error) {
+                    return console.log('Error:', error);
+                }
+
+                else {
+                    return cb(response, body);
+                }
+            });
     }
 
 }
--- terminalone/entity.ts
+++ terminalone/entity.ts
@@ -9,17 +9,17 @@ class Entity {
     private schema;
     private connection;
 
-    constructor(name: string, connection, id?: number) {
+    constructor(name:string, connection, id?:number) {
         this.data = {};
         this.meta = {};
         this.entity_type = name;
         this.connection = connection;
-        var fs = require("fs");
-        var schemaContent = fs.readFileSync("./schema/" + this.entity_type + ".json", "utf8");
+        const fs = require("fs");
+        const schemaContent = fs.readFileSync("./schema/" + this.entity_type + ".json", "utf8");
         this.schema = JSON.parse(schemaContent);
         if (id != null) {
             var that = this;
-            this.connection.get(entityMap[this.entity_type] + "/" + id, function(response, body) {
+            this.connection.get(entityMap[this.entity_type] + "/" + id, function (response, body) {
                 var content = JSON.parse(body);
                 console.log(content);
                 that.data = content.data;
@@ -32,8 +32,28 @@ class Entity {
         var Validator = require('jsonschema').Validator;
         var v = new Validator();
         var results = (v.validate(this, this.schema));
-        return results.errors.map(function(obj) {return obj.stack;})
+        return results.errors.map(function (obj) {
+            return obj.stack;
+        })
     }
+
+    save() {
+        var verification = this.ensure_valid();
+        if (verification.length == 0) {
+            var schema = this.schema.properties.data.properties;
+            var form = JSON.parse(JSON.stringify(this.data));
+            Object.keys(schema).forEach(function (key) {
+                if (schema[key].readonly) delete form[key];
+            });
+
+            this.connection.post(entityMap[this.entity_type] + "/" + this.data.id, form, console.log)
+        }
+        else {
+            console.log(verification.join(', '));
+        }
+    }
+
+
 }
 
 export = Entity;
