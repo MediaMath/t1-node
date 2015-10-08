409b4cf1fac87239100a0d52146321968a7722c7
Prasanna Swaminathan <prasanna@mediamath.com>
2015-10-08 10:50:49 -0400

    Fix reference path for typings

--- lib/references.ts
+++ lib/references.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../typings/node/node.d.ts"/>
+/// <reference path="../typings/node/node.d.ts"/>
 /// <reference path="t1connection.ts" />
 /// <reference path="entitymap.ts" />
 /// <reference path="entity.ts" />
