2a3726ced655470159716b9f466a2282c4471b1f
Fotios Tzellos <ftzellos@mediamath.com>
2015-10-29 11:58:02 +0000

    stop writing typescript.

---
+++ index.js
@@ -0,0 +1,2 @@
+exports.T1Connection = require('./lib/t1connection');
+exports.Entity = require('./lib/entity');
--- lib/references.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../typings/node/node.d.ts"/>
-/// <reference path="t1connection.ts" />
-/// <reference path="entitymap.ts" />
-/// <reference path="entity.ts" />
