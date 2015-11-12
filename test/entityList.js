89ccab9f47355ab9a4b56de93f01cebe6b9d21ac
Fotios Tzellos <ftzellos@mediamath.com>
2015-11-12 10:56:39 +0000

    make entityList return a generator

--- test/entityList.js
+++ test/entityList.js
@@ -5,48 +5,49 @@ var sinon = require('sinon');
 var t1 = require('../index');
 
 
-var loadFixture = function(fixtureName) {
+var loadFixture = function (fixtureName) {
     var fs = require("fs");
     return fs.readFileSync(__dirname + '/fixtures' + '/' + fixtureName + ".json", "utf8");
 };
 
 describe("entityList", function () {
 
-    var connectionStub =  {};
-    connectionStub.get = function() { };
-    connectionStub.post = function() { };
+    var connectionStub = {};
+    connectionStub.get = function () {
+    };
+    connectionStub.post = function () {
+    };
     var sandbox, getStub, postStub;
     var parsedResult = "aisdaiusd";
 
     var service = t1.EntityList.prototype;
     service.t1connection = connectionStub;
 
-    beforeEach(function() {
+    beforeEach(function () {
         sandbox = sinon.sandbox.create();
         getStub = sandbox.stub(connectionStub, "get").returns(Q(parsedResult));
         postStub = sandbox.stub(connectionStub, "post").returns(Q(parsedResult));
     });
 
-    afterEach(function() {
+    afterEach(function () {
         sandbox.restore();
     });
-
 
     describe("#get with count", function () {
         var userParams = {'page_limit': 10};
 
         parsedResult = loadFixture('campaigns-10');
 
-
         it("should have 10 entities", function () {
             var campaigns = service.get('campaigns', userParams);
+            expect(getStub.callCount).to.eventually.be.equal(1);
 
-            return expect(campaigns).to.eventually
-                    .have.property('entities')
-                    .and.have.length(userParams.page_limit) &&
-                expect(campaigns).to.eventually
-                    .have.property('entityCount', userParams.page_limit) &&
-                expect(campaigns).to.eventually.have.property('next_page')
+            expect(campaigns).to.eventually
+                .have.property('entities')
+                .and.have.length(userParams.page_limit);
+            expect(campaigns).to.eventually
+                .have.property('entityCount', userParams.page_limit);
+            expect(campaigns).to.eventually.have.property('next_page')
         });
     });
 
@@ -58,20 +59,16 @@ describe("entityList", function () {
 
         it("should have 100 entities", function () {
             var campaigns = service.get('campaigns', userParams);
+            expect(getStub.callCount).to.be.equal(1);
 
-            return expect(campaigns).to.eventually
-                    .have.property('entities')
-                    .and.have.length(expectedEntityCount) &&
-                expect(campaigns).to.eventually
-                    .have.property('entityCount', expectedEntityCount) &&
-                expect(campaigns).to.eventually.have.property('next_page')
+            expect(campaigns).to.eventually
+                .have.property('entities')
+                .and.have.length(expectedEntityCount);
+            expect(campaigns).to.eventually
+                .have.property('entityCount', expectedEntityCount);
+            expect(campaigns).to.eventually.have.property('next_page')
         });
 
     });
 });
-
-
-
-
-
 
